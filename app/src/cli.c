794758b329fbb4c60910a6be5073190fca1476fd
kuuldor <kuuldor@users.noreply.github.com>
2024-12-29 16:15:57 -0800

    Add option for touchmap file

--- app/src/cli.c
+++ app/src/cli.c
@@ -114,6 +114,7 @@ enum {
     OPT_NO_VD_SYSTEM_DECORATIONS,
     OPT_NO_VD_DESTROY_CONTENT,
     OPT_DISPLAY_IME_POLICY,
+    OPT_GAMEPAD_TOUCHMAP
 };
 
 struct sc_option {
@@ -670,6 +671,12 @@ static const struct sc_option options[] = {
         // deprecated
         .longopt_id = OPT_NO_DISPLAY,
         .longopt = "no-display",
+    },
+    {
+        .longopt_id = OPT_GAMEPAD_TOUCHMAP,
+        .longopt = "gamepad-touchmap",
+        .argdesc = "map_file",
+        .text = "Specify config file mapping game controller input to touch.",
     },
     {
         .longopt_id = OPT_NO_KEY_REPEAT,
@@ -2585,6 +2592,9 @@ parse_args_with_getopt(struct scrcpy_cli_args *args, int argc, char *argv[],
                 break;
             case OPT_NO_MIPMAPS:
                 opts->mipmaps = false;
+                break;
+            case OPT_GAMEPAD_TOUCHMAP:
+                opts->touchmap_file = optarg;
                 break;
             case OPT_NO_KEY_REPEAT:
                 opts->forward_key_repeat = false;
